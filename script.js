cefd3659c9890b26e1c2f74334ea480412458951
EljonB <eljonbostanxhi79@gmail.com>
2024-02-21 19:12:35 +0100

    New Style

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function openModal(img) {
-    const modal = document.getElementById("modal");
-    const modalImg = document.getElementById("modal-img");
-  
-    modal.style.display = "block";
-    modalImg.src = img.src;
-  }
-  
-  function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-  }
-  
+let currentIndex = 0;
+const slides = document.querySelector('.slider').children;
+const totalSlides = slides.length;
+
+function changeSlide(offset) {
+  currentIndex = (currentIndex + offset + totalSlides) % totalSlides;
+  updateSlider();
+}
+
+function updateSlider() {
+  const newPosition = -currentIndex * 100 + '%';
+  document.querySelector('.slider').style.transform = 'translateX(' + newPosition + ')';
+}
+
+document.querySelector('.prev-btn').addEventListener('click', function () {
+  changeSlide(-1);
+});
+
+document.querySelector('.next-btn').addEventListener('click', function () {
+  changeSlide(1);
+});
